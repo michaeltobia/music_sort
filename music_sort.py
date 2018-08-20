1cb441b358be9dbc4040c16a783f1f7061e7d91f
Michael Tobia <michaeltobia2019@u.northwestern.edu>
2018-08-20 00:01:38 -0500

    Converted back to move allows for reliable detection of duplicates. That said, it's very difficult to tell if any cover in _unsorted is a duplicate or just a cover that didn't make it into the correct album folder (especially when said cover lacks any significant wording that would point to where it belongs.) Consider renaming covers that are sent to unsorted with some combination of cur_artist and cur_album. clean_target() method was created in similar recursive fasion as sift() method. As of this commit clean_target() is untested. Added a small todo at the top of the page as well regarding creating methods for error handling instead of handling them on a case-by-case basis. The todo is specifically intended for duplicate handling.

--- music_sort.py
+++ music_sort.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+#### To Do
+####    - You should probably make a method for duplicate name altering/error handling
+####    - Make clean() method to remove empty folders after sort
+
 import os, sys, unicodedata, shutil     # native utilities
 import eyed3    # audio file ID3 package
 
@@ -51,7 +55,7 @@ class MusicSorter:
         ## order this function sifts in
         if len(contents) == 0: ## Notify if current dir is empty
             print('\n\n\n****EMPTY: ' + path + '\n\n\n')
-            pause_junk = input('Press enter to continue...')
+            pause_junk = raw_input('Press enter to continue...')
             # os.rmdir(path) ## delete current folder if empty
         for i in range(len(contents)):
             cur_path = path + '/' + contents[i]
@@ -76,7 +80,6 @@ class MusicSorter:
                 #     # print "ERROR: " + contents[i]
         # if self.dry_run == 'n':
             # os.rmdir(path)
-        self.return_log()
 
     # Still has trouble with split albums (albums that have multiple artists)
     def sort(self, path):
@@ -102,11 +105,16 @@ class MusicSorter:
                         os.makedirs(self.cur_album_path)
                     # move song to album path
                     try:
-                        shutil.copy(path, self.cur_album_path)
+                        shutil.move(path, self.cur_album_path)
                     except shutil.Error:
                         print("Duplicate error at: " + path + '\n')
-                        trash = input('Press enter to continue...')
-                        shutil.copy(path, self.path_error)
+                        trash = raw_input('Press enter to continue...')
+                        try:
+                            shutil.move(path, self.path_error)
+                        # allow for multiple duplicates
+                        except shutil.Error:
+                            dupe_name = '/' + os.path.basename(path) + str(self.duplicate_name_counter)
+                            shutil.move(path, self.path_error + dupe_name)
 
 
                 #Add song to sort_log
@@ -123,7 +131,7 @@ class MusicSorter:
                 # if tags have indecipherable characters, move song to _unsorted
                 print('ERROR: Unicode error at ' + path)
                 if self.dry_run == 'n':
-                    shutil.copy(path, self.path_error)
+                    shutil.move(path, self.path_error)
 
                 return
             except ValueError:
@@ -134,34 +142,53 @@ class MusicSorter:
             # print('COVER: ' + path + '\n')
             if self.dry_run == 'n':
                 try:
-                    shutil.copy(path, self.cur_album_path)
+                    shutil.move(path, self.cur_album_path)
                 except AttributeError:
-                    shutil.copy(path, self.path_error)
+                    shutil.move(path, self.path_error)
                     print('UNSORTED COVER: ' + path)
+                except shutil.Error:
+                    dupe_name = '/' + os.path.basename(path) + str(self.duplicate_name_counter)
+                    shutil.move(path, self.path_error + dupe_name)
+                    self.duplicate_name_counter += 1
         else:
             # move misc files (a la .ini, .txt, ect.) to _unsorted
             # print('TRASH: ' + path + '\n')
             if self.dry_run == 'n':
                 # move file to _unsorted
                 try:
-                    shutil.copy(path, self.path_error)
+                    shutil.move(path, self.path_error)
                 # rename file if a file with the same name is already in _unsorted
                 # then move it
                 except shutil.Error:
                     # os.rename(path, path + str(self.duplicate_name_counter))
-                    dupe_name = path + str(self.duplicate_name_counter)
-                    shutil.copy(path, dupe_name)
-                    shutil.move(dupe_name, self.path_error)
+                    dupe_name = '/' + os.path.basename(path) + str(self.duplicate_name_counter)
+                    shutil.move(path, self.path_error + dupe_name)
                     self.duplicate_name_counter += 1
                 # rename + move tree if 'file' is a trash dir
                 except IOError:
-                    shutil.copytree(path, path + str(self.duplicate_name_counter))
-                    shutil.move(path + str(self.duplicate_name_counter), self.path_error)
+                    # shutil.copytree(path, path + str(self.duplicate_name_counter))
+                    dupe_name = '/' + os.path.basename(path) + str(self.duplicate_name_counter)
+                    shutil.move(path, self.path_error + dupe_name)
                     self.duplicate_name_counter += 1
         return
 
-    def return_log(self):
-        return self.sort_log
+    def clean_target(self, path):
+        contents = os.listdir(path)
+        if len(contents) == 0:
+            os.rmdir(path)
+        else:
+            for i in range(len(contents)):
+                cur_path = path + '/' + contents[i]
+                if '_singles' in  or '_sorted' in cur_path or '_unsorted' in cur_path:
+                    continue
+                elif os.path.isdir(cur_path):
+                    self.clean_target(cur_path)
+                else:
+                    os.rmdir(cur_path)
+                    return
+        return
+
+
 
 
 if __name__ == "__main__":
